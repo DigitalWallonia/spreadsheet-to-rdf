72e2ddf4b3b0c66f119ebeaa2c83b3cf01942902
Emilien Caudron <129062731+Ecaudron@users.noreply.github.com>
2025-01-13 15:56:10 +0100

    updating tqdm progress bar

--- utils/transformer.py
+++ utils/transformer.py
@@ -1,4 +1,5 @@
 import os
+import time
 import json
 import logging
 from datetime import date
@@ -8,7 +9,7 @@ from tqdm import tqdm
 from utils.creating_triples import add_concept, add_conceptScheme, add_topConcept, ENGLISH_LABELS
 from utils.data_utils import shacl_validation, CHANGED_LABELS, find_duplicate_values, taxonomy_size_validation
 
-def adding_triples(taxo_excel: pd, taxo_graph: Graph, level: int, highest_level: str, column_names: dict, D4W_NAMESPACE: str, rules: list, default_language: str, default_version: str, create_english_labels: str, creation_date: str, default_status: str, checkmispell: str) -> None:
+def adding_triples(taxo_excel: pd, taxo_graph: Graph, level: int, highest_level: str, column_names: dict, D4W_NAMESPACE: str, rules: list, default_language: str, default_version: str, create_english_labels: str, creation_date: str, default_status: str, checkmispell: str, pbar: tqdm) -> None:
     """
     Adds RDF triples to a given RDF graph based on taxonomy data from an Excel file.  
   
@@ -51,7 +52,7 @@ def adding_triples(taxo_excel: pd, taxo_graph: Graph, level: int, highest_level:
     """
     unique_concepts = taxo_excel.drop_duplicates(subset=f"{column_names['Concept']}{level}")
     # Loop over concepts by level
-    for index in tqdm(unique_concepts.index, desc=f"Processing Level {level}", colour="green", leave=False):
+    for index in unique_concepts.index:
         if level > int(highest_level) + 1: 
             add_concept(taxo_graph, D4W_NAMESPACE, unique_concepts.loc[index], level, rules, default_language, default_version, create_english_labels, default_status, checkmispell, column_names)
         elif level == int(highest_level) + 1: 
@@ -59,6 +60,11 @@ def adding_triples(taxo_excel: pd, taxo_graph: Graph, level: int, highest_level:
         else: 
             add_conceptScheme(taxo_graph, D4W_NAMESPACE, unique_concepts.loc[index], level, rules, default_language, default_version, create_english_labels, creation_date, column_names)
 
+        # Updating the progress bar
+        if len(unique_concepts) < 200:
+            time.sleep(0.1)
+        pbar.update(1)
+        
 def excel_to_rdf(config: dict) -> None:
     """
     Converts an Excel file containing taxonomy data to an RDF file and validates the RDF using a SHACL API.  
@@ -106,16 +112,23 @@ def excel_to_rdf(config: dict) -> None:
     taxo_graph.bind("status", STATUS_NS)
     taxo_graph.bind("eurovoc", EUROVOC_NS)
 
-    for file_name in tqdm(os.listdir(input_folder), total=len(os.listdir(input_folder)), desc="Processing taxonomy", position=0, leave=True):
+    for file_name in tqdm(os.listdir(input_folder), total=len(os.listdir(input_folder)), desc="Processing taxonomy", position=0):
         # Read taxonomy from excel
         taxo_excel = pd.read_excel(os.path.join(input_folder, file_name))
         taxo_language = file_name[:2]
-
+        level_pbars = []  # Keep track of level progress bars
+        
         # Add triples to the rdf by level of the taxonomy
         for level in range(int(highest_level), int(lowest_level) + 1):
-
-            adding_triples(taxo_excel, taxo_graph, level, highest_level, column_names, D4W_NAMESPACE, rules, taxo_language, default_version, create_english_labels, creation_date, default_status, checkmispell) 
-                
+            # Create a progress bar for each level
+            pbar = tqdm(total=len(taxo_excel.drop_duplicates(subset=f"{column_names['Concept']}{level}").index), desc=f"Level {level}", leave=False, colour="green")  
+            level_pbars.append(pbar)
+            
+            adding_triples(taxo_excel, taxo_graph, level, highest_level, column_names, D4W_NAMESPACE, rules, taxo_language, default_version, create_english_labels, creation_date, default_status, checkmispell, pbar)
+        
+        for pbar in level_pbars:  
+            pbar.close()                 
+    
     for rule in rules:
         for rule_label in rule: 
             logging.info(f"Labels changed based on rule {rule_label}: {CHANGED_LABELS[rule_label]}")
